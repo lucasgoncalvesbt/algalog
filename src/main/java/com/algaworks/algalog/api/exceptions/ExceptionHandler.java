602cb3f6fa34e09a9ace34d23a79dc0e21d5e0a3
Lucas Gonçalves <lucasgoncalves.jobs@gmail.com>
2022-01-14 18:58:58 -0300

    Implementando Exception Handler

---
+++ src/main/java/com/algaworks/algalog/api/exceptions/ExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.algaworks.algalog.api.exceptions;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+@RestControllerAdvice
+public class ExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private MessageSource messageSource;
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers, HttpStatus status,
+            WebRequest request
+    ) {
+        List<Problema.Campo> campos = new ArrayList<>();
+
+        ex.getBindingResult().getFieldErrors()
+                .forEach(e -> {
+                    String nome = e.getField();
+                    String mensagem = messageSource.getMessage(e, LocaleContextHolder.getLocale());
+                    campos.add(new Problema.Campo(nome, mensagem));
+                });
+
+
+        Problema problema = new Problema();
+        problema.setStatus(status.value());
+        problema.setDataHora(LocalDateTime.now());
+        problema.setTitulo("Um ou mais campos inválidos na requisição");
+        problema.setCampos(campos);
+
+        return handleExceptionInternal(ex, problema, headers, status, request);
+    }
+}
