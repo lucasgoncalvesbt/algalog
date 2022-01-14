f856f785380027c57b091c19cd6ef702f59b3dc0
Lucas Gonçalves <lucasgoncalves.jobs@gmail.com>
2022-01-14 13:29:17 -0300

    Usando Spring Data JPA

--- src/main/java/com/algaworks/algalog/api/controllers/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controllers/ClienteController.java
@@ -1,6 +1,9 @@
 package com.algaworks.algalog.api.controllers;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repositories.ClienteRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,13 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 @RequestMapping("/clientes")
+@RestController
 public class ClienteController {
+
+    private final ClienteRepository clienteRepository;
 
     @GetMapping
     public ResponseEntity<List<Cliente>> listar() {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(clienteRepository.findAll());
     }
 
 }
